0e7203463a5c8536721b1b6c0cc0ad921918a805
USER <USER@DESKTOP-0P43UTC>
2026-09-04 16:21:26 +0530

    product,Added new POP class

---
+++ AdvanceSelenium_CRMGUIFrameWork_Git/src/main/java/com/comcast/crm/objectrepo/products.java
@@ -0,0 +1,21 @@
+package com.comcast.crm.objectrepo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class products {
+	@FindBy(xpath = "//img[@alt='Create Product...']")
+	private WebElement productimgbtn;
+	
+	public products(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement getProductimgbtn() {
+		return productimgbtn;
+	}
+	
+
+}
